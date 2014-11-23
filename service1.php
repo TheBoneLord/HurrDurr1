fcd8929e8b76c39f0b1ad8cac1f64d67bc0cc973
Bone <necron9631@yandex.ru>
2014-11-23 18:49:10 +0400

    s

---
+++ service1.php
@@ -0,0 +1,236 @@
+<?php header('Content-Type: text/html; charset=utf-8');?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Сервисная служба ИНФИНИТ  24ч – диагностика, ремонт, обслуживание силовой техники.</title>
+<meta charset="utf-8">
+<meta name="Description" content="Сервисная служба ИНФИНИТ представляет собой полноценную платформу длядиагностики, ремонта, сервисного и технического обслуживания силовой техники. Телефон круглосуточной службы поддержки: 8-800-100-29-63."/>
+<link rel="stylesheet" type="text/css" href="mail/css/bootstrap.min.css"/>
+<link href="/style.css" rel="stylesheet" type="text/css"/>
+<link href="/favicon.png" rel="shortcut icon" type="image/png" />
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+ <script src="mail/feedback/js/feedback.js"></script>
+    <script src="mail/feedback/js/jquery.arcticmodal.js"></script>
+    <script src="mail/feedback/js/jquery.jgrowl.js"></script>
+    <script type="text/javascript" src="/js/lightbox-2.6.min.js"></script>
+    <link type="text/css"  href="/css/lightbox.css" rel="stylesheet" />
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $(document).on('click', '.modal_btn', function(){
+            $('#small-modal').arcticmodal();
+        });
+    });
+    </script>
+ 
+    <link rel="stylesheet" type="text/css" href="mail/feedback/css/jquery.arcticmodal.css"/>
+    <link rel="stylesheet" type="text/css" href="mail/feedback/css/jquery.jgrowl.css"/>
+    <link rel="stylesheet" type="text/css" href="mail/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="/style.css"/>
+    <!--[if lt IE 7]>
+    <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
+    <![endif]-->
+     <!--[if lt IE 9]>
+    <script type="text/javascript" src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+    <script type="text/javascript">
+    var IE7_PNG_SUFFIX = ".png";
+    </script>
+    <style type="text/css">
+    .input-prepend input {
+    width:200px;
+    }
+    textarea {
+    width:250px;
+    }
+    </style>
+    <![endif]-->
+<style type="text/css">
+    .innertel{
+        background: #b8cbe4;
+        padding: 8px;
+        font-size: 28px;
+        font-family: Arial;
+        text-align: center;
+        letter-spacing: 1.2px; 
+    }
+        .innertel > .bold{
+            font-size: 32px;
+            font-weight: 900;
+        }
+    .block-about{
+        height: 30px;
+        font-weight: bolder;
+        background: #ffffff; /* Old browsers */
+        background: -moz-linear-gradient(top,  #ffffff 43%, #ffffff 60%, #969696 60%, #f2f2f2 61%, #ededed 62%, #ededed 62%, #ffffff 100%); /* FF3.6+ */
+        background: -webkit-gradient(linear, left top, left bottom, color-stop(43%,#ffffff), color-stop(60%,#ffffff), color-stop(60%,#969696), color-stop(61%,#f2f2f2), color-stop(62%,#ededed), color-stop(62%,#ededed), color-stop(100%,#ffffff)); /* Chrome,Safari4+ */
+        background: -webkit-linear-gradient(top,  #ffffff 43%,#ffffff 60%,#969696 60%,#f2f2f2 61%,#ededed 62%,#ededed 62%,#ffffff 100%); /* Chrome10+,Safari5.1+ */
+        background: -o-linear-gradient(top,  #ffffff 43%,#ffffff 60%,#969696 60%,#f2f2f2 61%,#ededed 62%,#ededed 62%,#ffffff 100%); /* Opera 11.10+ */
+        background: -ms-linear-gradient(top,  #ffffff 43%,#ffffff 60%,#969696 60%,#f2f2f2 61%,#ededed 62%,#ededed 62%,#ffffff 100%); /* IE10+ */
+        background: linear-gradient(to bottom,  #ffffff 43%,#ffffff 60%,#969696 60%,#f2f2f2 61%,#ededed 62%,#ededed 62%,#ffffff 100%); /* W3C */
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#ffffff',GradientType=0 ); /* IE6-9 */
+        text-align: center;
+        padding-top: 10px;
+        margin-bottom: 20px;
+    }
+        .block-about>span{
+            color: #717171;
+            font-size: 15px;
+            background-color: white;
+            padding: 5px;
+        }
+        .block-about>.large{
+            color: #000;
+            font-size: 20px;
+            background-color: white;
+            padding-top: -10px;
+        }
+    .block-images>li{
+        display: inline-block;
+        margin-right: 45px;
+        vertical-align: top;
+        text-align: center;
+    }
+    .block-images>li:last-child{
+        margin-right: 0px;
+    }
+        .block-images>li>img{
+            border: 2px solid #bab6b0;
+            border-radius: 5px;
+            margin: 5px 5px 5px 5px;
+        }
+        .block-images>li>p{
+            font-weight: bold;
+            text-align: center;
+            padding-top:10px;
+            color: #666666;
+        }
+        ul.block-images>li>span{
+            font-size: 10px;
+            color: #666666;
+        }
+</style>
+</head>
+
+	<body>
+<?php include "header.php";?>
+<div id="new"></div>
+
+<div style="height:8px"></div>
+<div class="box-content" style="padding: 20px;">
+<div style="float:right;margin-right:10px"><a href="/auth"><img src="/images/button_mos.JPG" alt="Личный кабинет"/></a></div>
+
+
+<div style="margin-top:10px;clear:both"></div>
+<div class="box-content" style="padding:5px;">
+    <div class="innertel">
+        ТЕЛЕФОН КРУГЛОСУТОЧНОЙ СЛУЖБЫ ПОДДЕРЖКИ
+        <div class="bold">8-800-100-29-63</div>
+    </div>
+</div>
+<div style="clear:both"></div>
+
+<div class="block-about">
+    <span>КАК МЫ РАБОТАЕМ?</span>
+</div>
+<ul class="block-images">
+    <li>
+        <img src="/images/service1.jpg" alt="">
+        <p>ВХОДЯЩИЙ ЗВОНОК</p>
+        <span>Входящий звонок или<br> заявка с сайта</span>
+    </li>
+    <li>
+        <img src="/images/service2.jpg" alt="">
+        <p>ВЫЕЗД СПЕЦИАЛИСТА</p>
+        <span>Выезд специалиста<br> на обследование Вашего дома</span>
+    </li>
+    <li>
+        <img src="/images/service3.jpg" alt="">
+        <p>ПОДГОТОВКА <br> ПРЕДЛОЖЕНИЯ</p>
+        <span>Установка генератора<br> на вашем участке</span>
+    </li>
+    <li>
+        <img src="/images/service4.jpg" alt="">
+        <p>МОНТАЖ</p>
+        <span>Свет в Вашем доме<br> ВСЕГДА</span>
+    </li>
+    <li>
+        <img src="/images/service5.jpg" alt="">
+        <p>ТЕХН. ОБСЛУЖИВАНИЕ</p>
+        <span>Установка генератора<br> на вашем участке</span>
+    </li>
+</ul>
+<ul class="block-images">
+    <li>
+        <div class="block-about">
+            <p>ВЫЕЗДНАЯ КОНСУЛЬТАЦИЯ</p>
+        </div> 
+        <img src="/images/Au1.jpg" alt="" align="left">
+        <div align="left" font-size: 15px; color: #666666;>
+            Решившись на покупку оборудования для обеспечения резервного
+            электроснабжения вашего загородного дома или дачи, не пренебрегайте
+            помощью специалистов в этой области. Уверенность в самостоятельном
+            правильном выборе генератора ИБП или системы альтернативной энергии 
+            может обернуться в пустую потраченными деньгами и временем.
+            Закажите выездную консультацию технического специалиста для подбора о
+            борудования и согласования всех нюансов установки.
+        </div>
+    </li>
+    <li>
+        <div class="block-about">
+            <p>ПОДГОТОВКА ПРЕДЛОЖЕНИЯ</p>
+        </div>
+        <img src="/images/Au2.jpg" alt="" align="right">
+        <div align="right" font-size: 15px; color: #666666;>
+            Проводя аналогию с эксплуатацией автомобиля, можно сказать, 
+            что обслуживание генератора вовремя и в срок - залог исправной работы генератора. 
+            Соблюдения требований завода изготовителя по проведению планового технического 
+            обслуживания, а также регламентых ремонтных работ позволят всегда быть уверенным 
+            в безотказной работе вашей установки.
+        </div>
+    </li>
+    <li>
+        <div class="block-about">
+            <p>МОНТАЖ И УСТАНОВКА</p>
+        </div>
+        <img src="/images/Au3.jpg" alt="" align="left">
+        <div align="left" font-size: 15px; color: #666666;>
+            Монтаж и установка электрогенератора являются важнейшими факторами
+            надежной и долговечной работы вашего оборудования и самой генераторной установки.
+            Монтаж разных типов генераторов отличаются друг от друга. 
+            Выделяя три типа генераторов - бензиновые, дизельные, газовые - 
+            и зная нюансы устройства и работы каждого из них, наши специалисты 
+            профессионально и в кратчайшие сроки установят, подключат и настроят 
+            Вашу генераторную установку.
+        </div>
+    </li>
+    <li>
+        <div class="block-about">
+            <p>ТЕХНИЧЕСКОЕ ОБСЛУЖИВАНИЕ</p>
+        </div>
+        <img src="/images/Au4.jpg" alt="" align="right">
+        <div align="right" font-size: 15px; color: #666666;>
+            Проводя аналогию с эксплуатацией автомобиля, 
+            можно сказать, что обслуживание генератора вовремя и в срок - 
+            залог исправной работы генератора. Соблюдения требований завода 
+            изготовителя по проведению планового технического обслуживания, 
+            а также регламентых ремонтных работ позволят всегда быть уверенным 
+            в безотказной работе вашей установки.
+        </div>
+    </li>
+</ul>
+
+<div class="block-about">
+    <span class="large">ВЫЕЗДНАЯ КОНСУЛЬТАЦИЯ</span>
+</div>    
+
+</div>
+</div>
+
+<div style="height:50px"></div>
+<?php include "footer.php";?>
+</div>
+
+</body>
+
+</html>
